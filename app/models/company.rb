02315cdb4670f5d2ce96194180e2da54ea01fba2
programen-gh <takayuki.tanazawa1026@gmail.com>
2021-01-18 16:19:48 +0900

    ユーザー管理機能の実装

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,6 @@ class Company < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true, length: {maximum: 20}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
     passwords:     'users/passwords',
     registrations: 'users/registrations'
   }
+  root to: 'services#index'
+  resources :companies, only: [:index]
 end
