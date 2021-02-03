2ff01dfa7d8c868ec807ae5e5092208263e3d963
programen-gh <takayuki.tanazawa1026@gmail.com>
2021-02-03 18:49:02 +0900

    update README.md

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Service.create!(company_id: 1, name: "極楽浄土", address: "東京", establishment: "1991-10-26", capacity: 100 , rooms: 100, phone: 1234567890, explanation: "天国",
-#   category_id: 2, prefecture_id: 2, lump_sum_min: 500, lump_sum_max: 500, monthly_sum_min: 10, monthly_sum_max: 10, age_id: 2, self_reliance: 1, support_one: 1, support_two: 1,
-#   nursing_one: 1, nursing_two: 1, nursing_three: 1, nursing_four: 1, nursing_five: 1, dementia: 1, guarantor: 1, welfare: 1, take_care: 1,
-#   care_food: 1, liquid_food: 1, rehabilitation: 1, dm: 1, gastrostomy: 1, tube_feeding: 1, stoma: 1, home_oxygen_therapy: 1, ventilator: 1, bedsore: 1,
-#   sputum_suction: 1, dialysis: 1, tracheostomy: 1, ivh: 1, catheter: 1, pacemaker: 1, als: 1, aspiration_pneumonia: 1, asthma: 1, parkinson_disease: 1,
-#   disuse_syndrome: 1, clinical_depression: 1, schizophrenia: 1, rheumatism: 1, osteoporosis: 1, cerebral_infarction: 1, myocardial_infarction: 1, cancer: 1, images: [File.open("./app/assets/images/test_image.png")])
+#  Service.create!(company_id: 1, name: "極楽浄土", address: "東京", establishment: "1991-10-26", capacity: 100 , rooms: 100, phone: 1234567890, explanation: "天国",
+#    category_id: 2, prefecture_id: 2, lump_sum_min: 500, lump_sum_max: 500, monthly_sum_min: 10, monthly_sum_max: 10, age_id: 2, self_reliance: 1, support_one: 1, support_two: 1,
+#    nursing_one: 1, nursing_two: 1, nursing_three: 1, nursing_four: 1, nursing_five: 1, dementia: 1, guarantor: 1, welfare: 1, take_care: 1,
+#    care_food: 1, liquid_food: 1, rehabilitation: 1, dm: 1, gastrostomy: 1, tube_feeding: 1, stoma: 1, home_oxygen_therapy: 1, ventilator: 1, bedsore: 1,
+#    sputum_suction: 1, dialysis: 1, tracheostomy: 1, ivh: 1, catheter: 1, pacemaker: 1, als: 1, aspiration_pneumonia: 1, asthma: 1, parkinson_disease: 1,
+#    disuse_syndrome: 1, clinical_depression: 1, schizophrenia: 1, rheumatism: 1, osteoporosis: 1, cerebral_infarction: 1, myocardial_infarction: 1, cancer: 1, images: open("#{Rails.root}/db/fixtures/test_image.png") )
 
+# Rack::Test::UploadedFile.new(File.join(Rails.root, 'app/assets/images/test_image.png'))
